22de3289420b19a750b00bb6dc1cda902775dd8a
samu-koi301 <167400620+samu-koi301@users.noreply.github.com>
2024-04-18 15:29:35 +0900

    Add files via upload

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,10 @@
+package text.kadai_015;
+public class CarExec_Chapter15{
+public static void main(String[]args) {
+	
+	Car_Chapter15 Car =new Car_Chapter15(1,10);
+			
+	Car.gearChange(3);
+	Car.run();
+}
+}
