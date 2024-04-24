35500af627923803db7253e2b3de2c319e9b1be7
samu-koi301 <167400620+samu-koi301@users.noreply.github.com>
2024-04-24 16:46:49 +0900

    Add files via upload

---
+++ kadai_026/JyankenExec_Chapter26.java
@@ -0,0 +1,15 @@
+package text.kadai_026;
+
+public class JyankenExec_Chapter26 {
+public static void main(String[]args) {
+	Jyanken_Chapter26 ja=new Jyanken_Chapter26();
+	
+	ja.getMyChoice();
+	
+	ja.getRandom();
+	
+	ja.playGame();
+	
+	
+}
+}
